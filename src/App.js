1aa14f110b4e600246023ffc648a974e84858ab1
Narmadhu <oknarmadhu10496@gmail.com>
2020-03-15 14:30:57 +0530

    deploy my todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [newTodo, setNewTodo] = useState("");
+  const [itemsList, setItemsList] = useState([
+    { key: 1, text: "Watering plants" },
+    { key: 2, text: "Do Yoga" }
+  ]);
+
+  const updateNewTodo = e => {
+    setNewTodo(e.target.value);
+  };
+
+  const getNewTodo = e => {
+    e.preventDefault();
+    if (newTodo === "") return;
+    setItemsList([{ key: Date.now(), text: newTodo }, ...itemsList]);
+    e.target.reset();
+  };
+
+  const removeTodo = key => {
+    setItemsList(itemsList.filter(itemList => itemList.key != key));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="todo-form" onSubmit={getNewTodo}>
+        <input
+          placeholder="Your todos..."
+          className="submit-bar"
+          onChange={updateNewTodo}
+        />
+        <button type="submit" className="submit-btn">
+          Add
+        </button>
+        <ul className="todo-list">
+          {itemsList.map(itemList => (
+            <li key={itemList.key} className="todo-items">
+              {itemList.text}
+              <a
+                className="delete"
+                href="#"
+                onClick={() => removeTodo(itemList.key)}
+              >
+                X
+              </a>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
